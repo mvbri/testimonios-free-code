bbd70803508b3ee2e419b0bd0c36dd72bfc8007f
maria <mvbri10@gmail.com>
2023-02-02 15:23:58 -0400

    Crea componente para testimonio y lo utiliza para recrea seccion de la web de testimonio de freecode

--- src/App.js
+++ src/App.js
@@ -4,7 +4,33 @@ import Testimonios from './components/Testimonios';
 function App() {
 	return (
 		<div className='App'>
-			<Testimonios />
+			<div className='main-container'>
+				<h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+				<Testimonios
+					name='Shawn Wang'
+					country='Singapur'
+					charge='Ingeniero de Software'
+					company='Amazon'
+					img='shawn'
+					testimony='Da miedo cambiar de carrera. Solo gané la confianza de que podía programar trabajando a través de los cientos de horas de lecciones gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis cifras como ingeniero de software. freeCodeCamp cambió mi vida.'
+				/>
+				<Testimonios
+					name='Sarah Chima'
+					country='Nigeria'
+					charge='Ingeniera de Software'
+					company='ChatDesk'
+					img='sarah'
+					testimony='freeCodeCamp fue la puerta de entrada a mi carrera como desarrollador de software. El plan de estudios bien estructurado llevó mis conocimientos de programación de un nivel de principiante total a un nivel muy seguro. Era todo lo que necesitaba para conseguir mi primer trabajo de desarrollador en una empresa increíble.'
+				/>
+				<Testimonios
+					name='Emma Bostian'
+					country='Suecia'
+					charge='Ingeniera de Software'
+					img='emma'
+					company='Spotify'
+					testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+				/>
+			</div>
 		</div>
 	);
 }
--- src/components/Testimonios.js
+++ src/components/Testimonios.js
@@ -1,23 +1,24 @@
 // rfce - Para crear estructura básica de componente.
 import React from 'react';
 
-function Testimonios() {
+import '../stylesheet/Testimonios.css';
+
+function Testimonios(props) {
 	return (
 		<div className='container-testimony'>
 			<img
 				className='img-testimony'
-				src={require('../img/testimony-1.png')}
+				src={require(`../img/testimony-${props.img}.png`)}
 				alt='Foto de 1'
 			/>
 			<div className='container-text-testimony'>
-				<p className='name-testimony'>Lorem en ipsu</p>
-				<p className='charhe-name'>lorem ipsum</p>
-				<p className='text-testimony'>
-					Da miedo cambiar de carrera. Solo gané la confianza de que podía
-					programar trabajando a través de los cientos de horas de lecciones
-					gratuitas en freeCodeCamp. Dentro de un año tuve un trabajo de seis
-					cifras como ingeniero de software. freeCodeCamp cambió mi vida.
+				<p className='name-testimony'>
+					{props.name} en {props.country}
 				</p>
+				<p className='name-charge'>
+					{props.charge} en {props.company}
+				</p>
+				<p className='text-testimony'>"{props.testimony}"</p>
 			</div>
 		</div>
 	);
